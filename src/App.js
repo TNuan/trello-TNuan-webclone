91d644bcc6694d03cd9c1b6c4deec6fb4c651fe9
TNuan <minibike090@gmail.com>
2022-06-06 19:12:46 +0700

    Design trello Front-end.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,189 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="head">Hello world</h1>
+    <div className="trello-container">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header className="">
+            brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2015/05/31/11/45/journal-791286_960_720.jpg" alt="nuan-alt-img"/>
+              
+              title: TNUANDEV
+            </li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+          </ul>
+
+          <footer>
+            Add another card
+          </footer>
+        </div>
+
+        <div className="column">
+          <header className="">
+            brainstorm
+          </header>
+          <ul>
+            <li>
+              {/* <img src="https://cdn.pixabay.com/photo/2015/05/31/11/45/journal-791286_960_720.jpg" alt="nuan-alt-img"/> */}
+              
+              title: TNUANDEV
+            </li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+          </ul>
+
+          <footer>
+            Add another card
+          </footer>
+        </div>
+
+        <div className="column">
+          <header className="">
+            brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2015/05/31/11/45/journal-791286_960_720.jpg" alt="nuan-alt-img"/>
+              
+              title: TNUANDEV
+            </li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+          </ul>
+
+          <footer>
+            Add another card
+          </footer>
+        </div>
+
+        <div className="column">
+          <header className="">
+            brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2015/05/31/11/45/journal-791286_960_720.jpg" alt="nuan-alt-img"/>
+              
+              title: TNUANDEV
+            </li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+          </ul>
+
+          <footer>
+            Add another card
+          </footer>
+        </div>
+
+        <div className="column">
+          <header className="">
+            brainstorm
+          </header>
+          <ul>
+            <li>
+              {/* <img src="https://cdn.pixabay.com/photo/2015/05/31/11/45/journal-791286_960_720.jpg" alt="nuan-alt-img"/> */}
+              
+              title: TNUANDEV
+            </li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+          </ul>
+
+          <footer>
+            Add another card
+          </footer>
+        </div>
+
+        <div className="column">
+          <header className="">
+            brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2015/05/31/11/45/journal-791286_960_720.jpg" alt="nuan-alt-img"/>
+              
+              title: TNUANDEV
+            </li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+          </ul>
+
+          <footer>
+            Add another card
+          </footer>
+        </div>
+
+        <div className="column">
+          <header className="">
+            brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2015/05/31/11/45/journal-791286_960_720.jpg" alt="nuan-alt-img"/>
+              
+              title: TNUANDEV
+            </li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+          </ul>
+
+          <footer>
+            Add another card
+          </footer>
+        </div>
+
+        <div className="column">
+          <header className="">
+            brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://cdn.pixabay.com/photo/2015/05/31/11/45/journal-791286_960_720.jpg" alt="nuan-alt-img"/>
+              
+              title: TNUANDEV
+            </li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+            <li>add a list item in here</li>
+          </ul>
+
+          <footer>
+            Add another card
+          </footer>
+        </div>
+
+        
+      </div>
     </div>
   );
 }
