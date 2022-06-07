3ac17021573f8f60806446422fa55b4cc17f2842
TNuan <minibike090@gmail.com>
2022-06-07 16:54:36 +0700

    Implement Drag & Drop with smooth dnd

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,10 +5,10 @@ import './Card.scss'
 function Card(props) {
   const { card } = props
   return (
-    <li className="card-item">
-      {card.cover && <img src={card.cover} className="card-cover" alt="nuan-alt-img"/>}
+    <div className="card-item">
+      {card.cover && <img src={card.cover} onMouseDown={e => e.preventDefault()} className="card-cover" alt="nuan-alt-img"/>}
       {card.title}
-    </li>
+    </div>
   )
 }
 
