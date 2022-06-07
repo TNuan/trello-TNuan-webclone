1a3d57961a0169ed4a0e473b6106fb6139f40580
TNuan <minibike090@gmail.com>
2022-06-07 08:18:39 +0700

    Initial sample Data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { isEmpty } from 'lodash';
 
 import './BoardContent.scss';
-import Column from 'components/Column/Column';
 
+import Column from 'components/Column/Column';
+import { mapOrder } from 'utillities/sort';
+
+import { initialData } from 'actions/initialData';
 
 function BoardContent() {
+    const [board, setBoard] = useState({});
+    const [columns, setColumn] = useState([]);
+
+    useEffect(() => {
+        const boardFromDB = initialData.board.find(board => board.id === 'board-1');
+        if (boardFromDB) {
+            setBoard(boardFromDB);
+
+            setColumn(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'));
+        }
+    }, [])
+
+    if (isEmpty(board)) {
+        return ( <div className="not-found">Board not found</div> );
+    }
+
     return (
         <div className="board-content">
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
+            {columns.map((column, index) => <Column key={index} column={column} />)}
+            
         </div>
     )
 }
---
+++ src/utillities/sort.js
@@ -0,0 +1,11 @@
+/**
+ *  Created by NTNuan' author on 07/06/2022
+ *  -------------------------------
+ *  Order an array of objects based on another array order
+ *  ---
+*/ 
+
+export const mapOrder = (array, order, key) => {
+    array.sort((a, b) => order.indexOf(a[key]) - order.indexOf(b[key]));
+    return array;
+}
