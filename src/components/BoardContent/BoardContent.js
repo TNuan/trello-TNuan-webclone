4b1bec2d536837160db927d19b6a6d3869ac28c7
TNuan <minibike090@gmail.com>
2022-06-08 19:36:30 +0700

    Handle State Change.

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -6,6 +6,7 @@ import './BoardContent.scss'
 
 import Column from 'components/Column/Column'
 import { mapOrder } from 'utillities/sort'
+import { applyDrag } from 'utillities/dragDrop'
 
 import { initialData } from 'actions/initialData'
 
@@ -27,7 +28,28 @@ function BoardContent() {
   }
 
   const onColumnDrop = (dropResult) => {
-    console.log(dropResult)
+    // console.log(dropResult)
+    let newColumns = [...columns]
+    newColumns = applyDrag(newColumns, dropResult)
+
+    let newBoard = { ...board }
+    newBoard.columnOrder = newColumns.map(col => col.id)
+    newBoard.columns = newColumns
+    setColumn(newColumns)
+    setBoard(newBoard)
+  }
+
+  const onCardDrop = (columnId, dropResult) => {
+    if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
+      let newColumns = [...columns]
+
+      let currentColumn = newColumns.find(col => col.id === columnId)
+
+      currentColumn.cards = applyDrag(currentColumn.cards, dropResult)
+      currentColumn.cardOrder = currentColumn.cards.map(item => item.id)
+
+      setColumn(newColumns)
+    }
   }
 
   return (
@@ -45,11 +67,13 @@ function BoardContent() {
       >
         {columns.map((column, index) => (
           <Draggable key={index}>
-            <Column column={column} />
+            <Column column={column} onCardDrop={onCardDrop}/>
           </Draggable>
         ))}
       </Container>
-
+      <div className="add-new-column">
+        <i className="fa fa-plus icon" /> Add new column
+      </div>
     </div>
   )
 }
---
+++ src/utillities/dragDrop.js
@@ -0,0 +1,21 @@
+/**
+ * Created by NTNuan's author on 06/08/2022
+*/
+
+export const applyDrag = (arr, dragResult) => {
+  const { removedIndex, addedIndex, payload } = dragResult
+  if (removedIndex === null && addedIndex === null) return arr
+
+  const result = [...arr]
+  let itemToAdd = payload
+
+  if (removedIndex !== null) {
+    itemToAdd = result.splice(removedIndex, 1)[0]
+  }
+
+  if (addedIndex !== null) {
+    result.splice(addedIndex, 0, itemToAdd)
+  }
+
+  return result
+}
