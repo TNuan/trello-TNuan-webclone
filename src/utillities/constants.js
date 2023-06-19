6bcd7e28ba9f2f71387fd0634cb9983d2cb1e3e4
Nguyễn Thành Luân <minibike090@gmail.com>
2023-06-19 18:01:06 +0700

    fix route api

--- src/utillities/constants.js
+++ src/utillities/constants.js
@@ -2,7 +2,7 @@
  *  Created by NTNuan's author on 06/10/2022
  */
 
-export const API_ROOT = 'https://trello-t-nuan-api.vercel.app/'
+export const API_ROOT = 'https://trello-t-nuan-api.vercel.app'
 
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
