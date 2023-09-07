88839cddc7d5276e78efe48197346f3c97dc073f
Nguyễn Thành Luân <minibike090@gmail.com>
2023-09-07 21:31:36 +0700

    add member to board

--- src/components/BoardBar/BoardBar.js
+++ src/components/BoardBar/BoardBar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Navbar, Container, NavDropdown } from 'react-bootstrap'
+import { MODAL_ACTION_CONFIRM } from 'utillities/constants'
+import AddMemberModal from 'components/Common/AddMember/AddMemberModal'
 
 import './BoardBar.scss'
 
 function BoardBar() {
+  const [modalMemberShow, setModalMemberShow] = useState(false)
+
+  const onAddMembers = (type, newMemberOrder) => {
+    if (type === MODAL_ACTION_CONFIRM) {
+      // Handle and call api
+    }
+
+    //Close modal
+    setModalMemberShow(false)
+  }
+
   return (
     <nav className="navbar-board">
       <Navbar>
@@ -12,7 +25,7 @@ function BoardBar() {
           <Navbar.Toggle />
           <Navbar.Collapse className="justify-content-space">
             <NavDropdown title="" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Add member</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => setModalMemberShow(true)}>Add member</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Change visibility
               </NavDropdown.Item>
@@ -28,6 +41,11 @@ function BoardBar() {
           </Navbar.Text>
         </Container>
       </Navbar>
+
+      <AddMemberModal
+        modalShow={modalMemberShow}
+        onAddMembers={onAddMembers}
+      />
     </nav>
   )
 }
